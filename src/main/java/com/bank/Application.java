a49c53247e1cb287c7f26805babf019477451412
suraj1947 <suraj1947>
2022-06-01 17:10:33 +0000

    Initial commit

---
+++ src/main/java/com/bank/Application.java
@@ -0,0 +1,9 @@
+package com.bank;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
